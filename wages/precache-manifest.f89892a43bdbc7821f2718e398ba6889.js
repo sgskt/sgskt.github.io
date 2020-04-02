7572851c190f7d10c4fc9347fb5a5a7a08794104
Simon Georges-Kot <simon.georgeskot@gmail.com>
2020-04-02 17:29:23 +0200

    try wage

---
+++ wages/precache-manifest.f89892a43bdbc7821f2718e398ba6889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae5ce716a23616faa53e729e4c30825",
+    "url": "/wages/index.html"
+  },
+  {
+    "revision": "ce5c346b38570498914f",
+    "url": "/wages/static/css/main.7cb0305c.chunk.css"
+  },
+  {
+    "revision": "c4a854d6f7ae843177ed",
+    "url": "/wages/static/js/2.4c4ab0b3.chunk.js"
+  },
+  {
+    "revision": "a48240da3ae17e1a702b7164a639b62c",
+    "url": "/wages/static/js/2.4c4ab0b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5c346b38570498914f",
+    "url": "/wages/static/js/main.6593ef01.chunk.js"
+  },
+  {
+    "revision": "b77284de533289feee45",
+    "url": "/wages/static/js/runtime-main.12dbc7d6.js"
+  }
+]);
